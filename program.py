666bd81bf4fd4472c6078b468d48ca3baa7d18ad
Martin Becker <martin.becker@arvato.com>
2020-07-08 10:40:04 +0200

    Code ein wenig angepasst um statistische Werte auslesen zu können

--- program.py
+++ program.py
@@ -6,9 +6,11 @@ myTranslationsFlipped = {}
 def get_key_values(myDict, prevKey):
     for k, v in myDict.items():
         if isinstance(v, dict):
-            get_key_values(v, prevKey + '.' + k)
+            if prevKey == '':
+                get_key_values(v, k)
+            else:
+                get_key_values(v, prevKey + '.' + k)
         else:
-            #print(prevKey + '.' + k, v)
             myTranslations[prevKey + '.' + k] = v
 
 def generate_flipped_dictionary(myDict):
@@ -19,9 +21,19 @@ def generate_flipped_dictionary(myDict):
             myTranslationsFlipped[value].append(key)
 
 def get_multi_assigned_values(myDict):
-    for key, value in myDict.items():
-        if len(value)>1:
-            print(key, value)
+    f = open("duplicates.de.txt", "a")
+    duplicates = 0
+    items_with_duplicates = 0;
+    for key, values in myDict.items():
+        if len(values)>1:
+            items_with_duplicates += 1
+            duplicates += len(values)
+            my_values_string = ';'.join(values)
+            f.write(key + ':' + '['+ my_values_string + ']\n')
+    avg_duplicates = duplicates / items_with_duplicates
+    print("duplicates: ", duplicates)
+    print("avg duplicates: ", avg_duplicates)
+    f.close()
 
 with open('de.json') as json_file:
     data = json.load(json_file)
